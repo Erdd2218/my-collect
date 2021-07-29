fd43718abf17282be466292496868c439cc505e8
erdd2218 <github email address>
2021-07-28 23:58:52 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(empty_array)
+    counter = 0
+    new_arr = []
+    
+    while counter < empty_array.length
+        new_arr << yield(empty_array[counter])
+        counter += 1
+    end
 
+    new_arr
+end 
 
